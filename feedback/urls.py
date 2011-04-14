30bdbdb8c025181937190320377e4f984a2e4fab
Lior Sion <liorsion@the-macbook-air.(none)>
2011-04-14 19:41:06 +0300

    readme fix changing to ajax and views

--- feedback/urls.py
+++ feedback/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import *
 from feedback.views import leave_feedback
 
 urlpatterns = patterns('',
-    url(r'^$', leave_feedback, name='leave-feedback'),
+    url(r'^$', leave_feedback.as_view(), name='leave-feedback'),
 )
--- feedback/views.py
+++ feedback/views.py
@@ -1,15 +1,22 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.views.generic import View
+from django.utils import simplejson
 
 from feedback.forms import FeedbackForm
 
-def leave_feedback(request):
-    form = FeedbackForm(request.POST or None)
-    if form.is_valid():
-        feedback = form.save(commit=False)
-        feedback.user = request.user
-        feedback.save()
-        request.user.message_set.create(message="Your feedback has been saved successfully.")
-        return HttpResponseRedirect(request.POST.get('next', request.META.get('HTTP_REFERER', '/')))
-    return render_to_response('feedback/feedback_form.html', {'form': form}, context_instance=RequestContext(request))
+class leave_feedback(View):
+    
+    def post(self, request, *args, **kwargs):
+        form = FeedbackForm(request.POST)
+        result = {"success":False}
+        if form.is_valid():
+            feedback = form.save(commit=False)
+            feedback.user = request.user
+            feedback.save()
+            request.user.message_set.create(message="Your feedback has been saved successfully.")
+            
+            result = {"success":True,"message":"Thank You"}
+        return HttpResponse(simplejson.dumps(result), mimetype='application/json') 
+    
