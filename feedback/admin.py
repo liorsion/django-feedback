ed959a8b473ec2b9762f5954957faab2dd3701ab
Lior Sion <liorsion@the-macbook-air.(none)>
2011-04-27 15:06:05 +0300

    added status and command to admin views

--- feedback/admin.py
+++ feedback/admin.py
@@ -10,10 +10,11 @@ class FeedbackAdmin(admin.ModelAdmin):
     model = Feedback
     change_form_template = "feedback/view_feedback.html"
     
-    list_display = ['user', 'message', 'time', 'type', 'view']
+    list_display = ['user', 'message', 'time', 'type', 'status', 'view']
     search_fields = ['user', 'message']
-    list_filter = ['type', 'time']
-
+    list_filter = ['type', 'time', 'status']
+    actions = ['mark_fixed']
+    
     def view(self, obj):
         return "<a href='%s'>View</a>" % obj.get_absolute_url()
     
@@ -28,7 +29,17 @@ class FeedbackAdmin(admin.ModelAdmin):
         
     def view_feedback(self, request, feedback_id):
         return self.change_view(request, feedback_id)
-
+    
+    def mark_fixed(modeladmin, request, queryset):
+        rows_updated = queryset.update(status='F')
+        if rows_updated == 1:
+            message_bit = "1 story was"
+        else:
+            message_bit = "%s stories were" % rows_updated
+        self.message_user(request, "%s successfully marked as fixed." % message_bit)
+    
+    mark_fixed.short_description = "Mark selected feedback as fixed"
+    
 admin.site.register(Feedback, FeedbackAdmin)
 
 admin.site.index_template = 'feedback/index.html'
--- feedback/models.py
+++ feedback/models.py
@@ -4,15 +4,20 @@ from django.core.urlresolvers import reverse
 from django.conf import settings
     
 class Feedback(models.Model):
+    STATUS_TYPE_CHOICES = (
+        ('N', 'New'),
+        ('O', 'Open'),
+        ('F', 'Fixed')
+    )
     
     class Meta:
-        
         ordering = ['-time']
     
     user    = models.ForeignKey(User)
     type    = models.CharField(choices=settings.FEEDBACK_CHOICES, max_length=100)
     message = models.TextField()
     time    = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=1, choices=STATUS_TYPE_CHOICES, default='N', blank=True, null=True)
     
     def __unicode__(self):
         return self.message
